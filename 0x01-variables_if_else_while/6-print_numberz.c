3d5192bc9c9b562dd3994d3a48d5dc5aff0f6315
SoftKratos <e.olad@yahoo.com>
2023-03-20 06:03:58 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 /**
   *main - Entry point
-  *Description: 'Print alphabets lowercase'
+  *Description: 'Print number of base 10 using putchar'
   *Return: Always 0
   */
 int main(void)
 {
-	int n = 97;
+	int n;
 
-	while (n <= 122)
+	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
-		n++;
+		putchar(n + 48);
 	}
 	putchar('\n');
 	return (0);
 }
+
