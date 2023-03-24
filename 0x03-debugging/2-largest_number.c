681ac69769ebfc16acbfc18bb8e4a5218aad1b28
SoftKratos <e.olad@yahoo.com>
2023-03-24 16:20:02 -0700

    0x03-debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,6 +9,7 @@
 int largest_number(int a, int b, int c)
 {
 	int largest;
+
 	if (a > b && a > c)
 	{
 	largest = a;
@@ -18,7 +19,7 @@ int largest_number(int a, int b, int c)
 		largest = c;
 	}
 	else if (b > c)
-	{	
+	{
 		largest = b;
 	}
 	else
